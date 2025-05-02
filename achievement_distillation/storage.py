01768d46714374d7abb46c6c90738aa8a90cd9d7
msaeidgh71 <124666136+msaeidgh71@users.noreply.github.com>
2025-05-02 23:04:05 +0200

    Update storage.py

--- achievement_distillation/storage.py
+++ achievement_distillation/storage.py
@@ -26,6 +26,8 @@ class RolloutStorage:
 
         # Get obs shape and action dim
         assert isinstance(observation_space, spaces.Box)
+        print("*********");
+        print(action_space);
         assert isinstance(action_space, spaces.Discrete)
         obs_shape = getattr(observation_space, "shape")
         action_shape = (1,)
